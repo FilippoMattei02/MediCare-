08e9883c58b8f911e53c9d51e48530430f189635
pierzeno <pgseno02@gmail.com>
2024-05-31 14:40:07 +0200

    first commit

--- backend/app.js
+++ backend/app.js
@@ -18,6 +18,7 @@ require('dotenv').config({ path: path.join(__dirname, '..', '.env') });
 const authentication = require('./authentication');
 const holiday=require('./holidays');
 const Holidays = require('./models/holidays');
+const shifts = require('./shifts');
 const employees = require('./employees');
 
 
@@ -43,6 +44,7 @@ app.use('/auth', authentication);
 
 app.use('/holiday',holiday);
 app.use('/employees', employees);
+app.use('/shifts',shifts);
 
 app.listen(port, () => {
     console.log(`Server lisening to port: ${port}`);
---
+++ backend/shifts.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const router = express.Router();
+const Employee = require('./models/employee');
+require('dotenv').config();
+
+async function getWorkByUsername(username) {
+    try {
+        console.log(`Cerco lavori per username: ${username}`);
+        
+        // Trova l'utente con lo username specificato e ottieni il ruolo
+        const query = { username: username };
+        const projection = { work: 1, role: 1, _id: 0 }; // Aggiungi 'role' alla proiezione
+
+        const result = await Employee.findOne(query).select(projection).lean();
+
+        if (!result) {
+            console.log('Nessun utente trovato con questo username.');
+            return [];
+        }
+
+        const role = result.role;
+        console.log(`Ruolo trovato: ${role}`);
+
+        // Trova tutti gli utenti con lo stesso ruolo e ottieni i campi 'work' e 'username'
+        const usersWithSameRole = await Employee.find({ role: role }).select({ work: 1, username: 1, _id: 0 }).lean();
+
+        // Creare un array di oggetti contenenti 'username' e 'work'
+        const workWithUsernames = usersWithSameRole.map(user => ({
+            username: user.username,
+            work: user.work
+        }));
+
+        return workWithUsernames;
+
+    } catch (error) {
+        console.error(`Errore durante la ricerca dei lavori: ${error.message}`);
+        throw error;
+    }
+}
+
+router.get('/:username', async (req, res) => {
+    const username = req.params.username;
+
+    if (!username) {
+        return res.status(400).send('Username è richiesto');
+    }
+
+    try {
+        const tasks = await getWorkByUsername(username);
+
+        if (tasks && tasks.length > 0) {
+            // Log dettagliato di ogni utente e il loro work
+            tasks.forEach(user => {
+                console.log(`Username: ${user.username}`);
+                console.log(`Work: ${JSON.stringify(user.work, null, 2)}`);
+            });
+            return res.status(200).json(tasks);
+        } else {
+            return res.status(404).send('Nessun task trovato');
+        }
+    } catch (error) {
+        console.error('Errore nel gestire la richiesta /shift:', error);
+        return res.status(500).send('Errore del server');
+    }
+});
+
+
+module.exports = router;
