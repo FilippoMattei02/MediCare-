261c628a7257b569e17b7e5e8ec170b456691e5b
piegian99ita <pietro.giannini@studenti.unitn.it>
2024-06-25 14:09:45 +0200

    debug automate

--- frontend/javascript/shifts.js
+++ frontend/javascript/shifts.js
@@ -273,6 +273,7 @@ async function automateAndPublishShifts(role, year, month) {
             method: 'PUT'
         });
         console.log(`Automate response status: ${automateResponse.status}`);
+        console.log(automateResponse.error);
         if (!automateResponse.ok) {
             throw new Error('Error in automate request: ' + automateResponse.status);
         }
