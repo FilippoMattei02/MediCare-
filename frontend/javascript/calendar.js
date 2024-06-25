071091ba7fcb10cfdb426d920de8e292fb507b1f
pierzeno <pgseno02@gmail.com>
2024-06-25 11:46:46 +0200

    change frontend's calendar

--- frontend/javascript/calendar.js
+++ frontend/javascript/calendar.js
@@ -1,9 +1,9 @@
 let Helper;
-//API per prendere i dati
+let currentWeekStart = getStartOfTheWeek(new Date()); // Data di inizio della settimana corrente
+
+// API per prendere i dati
 async function connector() {
     const currentToken = localStorage.getItem('token');
-    console.log("currentToken: ", currentToken);
-
     try {
         const response = await fetch('https://medicare-p67f.onrender.com/auth/tokens', {
             method: 'POST',
@@ -14,156 +14,106 @@ async function connector() {
         });
 
         if (!response.ok) {
-            throw new Error('Network error: ' + response.status);
+            throw new Error('Errore di rete: ' + response.status);
         }
 
         const data = await response.json();
-        console.log("Data message", data.message);
         Helper = data.message;
         return Helper; // Restituisce l'username
     } catch (error) {
-        console.error('Error during login: ', error);
-        alert("Wrong credentials or network error.");
+        console.error('Errore durante il login:', error);
+        alert("Credenziali errate o errore di rete.");
         return null;
     }
 }
-
-
 
 async function fetchTasks(username) {
-    console.log(`Fetching tasks for username: ${username}`);
+    //const currentToken = localStorage.getItem('token');
     try {
-        const response = await fetch(`https://medicare-p67f.onrender.com/calendar/${username}`);
-        console.log(`Response status: ${response.status}`);
+        const response = await fetch(`https://medicare-p67f.onrender.com/calendar/${username}`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                //'Authorization': currentToken
+            }
+        });
+
         if (!response.ok) {
-            throw new Error('Request error: ' + response.status);
+            throw new Error('Errore nella richiesta: ' + response.status);
         }
         const tasks = await response.json();
-        console.log('Tasks received:', tasks);
-        return tasks;
+
+        let adjustedTasks = [];
+
+        for (let task of tasks) {
+            let startHour = task.start;
+            let endHour = task.end;
+            let taskDay = new Date(task.day);
+
+            if (startHour > endHour) {
+                // Split the task into two separate tasks
+                let nextDay = new Date(taskDay);
+                nextDay.setDate(nextDay.getDate() + 1);
+
+                // First task: from startHour to 24:00 of the same day
+                adjustedTasks.push({
+                    day: taskDay.toISOString().split('T')[0], // Save only the date part
+                    start: startHour,
+                    end: 24 // Cap the end at 24 hours for the current day
+                });
+
+                // Second task: from 00:00 to endHour of the next day
+                adjustedTasks.push({
+                    day: nextDay.toISOString().split('T')[0], // Save only the date part
+                    start: 0,
+                    end: endHour
+                });
+            } else {
+                // No need to split the task
+                adjustedTasks.push(task);
+            }
+        }
+
+        // Sort the tasks by day in ascending order
+        adjustedTasks.sort((a, b) => new Date(a.day) - new Date(b.day));
+
+        return adjustedTasks;
     } catch (error) {
-        console.error('Loading task error', error);
+        console.error('Errore nel caricamento dei task', error);
         return null;
     }
 }
-
-
 
 // Funzione per aggiungere un task a una cella specifica
 function addTask(calendar, day, timeSlot, user) {
     const formattedUser = formatUsername(user);
-    const cellId = calendar === "current" ? `${day}-${timeSlot}` : `${day}-${calendar}-${timeSlot}`;
+    const cellId = `${day}-${calendar}-${timeSlot}`;
+    console.log(`Adding task to cell: ${cellId}`);
     const cell = document.getElementById(cellId);
     if (cell) {
-        cell.innerHTML += `<div> <span class="user">(${formattedUser})</span></div>`;
+        cell.innerHTML += `<div><span class="user">(${formattedUser})</span></div>`;
     }
-}
-//funzioni potenzialmente funzionanti
-
-function getPreviousDay(date) {
-    let previousDay = new Date(date);
-    previousDay.setDate(previousDay.getDate() - 1);
-    return previousDay;
-}
-function getNextDay(date) {
-    let nextDay = new Date(date);
-    nextDay.setDate(nextDay.getDate() + 1);
-    return nextDay;
 }
 
 function getStartOfTheWeek(date) {
-    let date1 = new Date(date);
-    let day = date1.getDay();
-    if (day == 1) {
-        return date1;
-    } else {
-        while (day != 1) {
-            date1 = getPreviousDay(date1);
-            day = date1.getDay();
-        }
-        return date1;
-    }
+    const currentDate = new Date(date);
+    const day = currentDate.getUTCDay();
+    const difference = day === 0 ? 0 : -day; // Sunday (0) is the start of the week
+    currentDate.setUTCDate(currentDate.getUTCDate() + difference);
+    currentDate.setUTCHours(0, 0, 0, 0); // Reset time to midnight in UTC
+    return currentDate;
 }
 
 function getEndOfTheWeek(date) {
-    let date2 = new Date(date);
-    let day = date2.getDay();
-    if (day == 0) {
-        return date2;
-    } else {
-        while (day != 0) {
-            date2 = getNextDay(date2);
-            day = date2.getDay();
-        }
-        return date2;
-    }
-}
-function isCurrent(date){
-let datealt=new Date();
-var datealt2=new Date(date);
-console.log(datealt2);
-var date1=getEndOfTheWeek(datealt2);
-var date2=getEndOfTheWeek(datealt);
-
-if(date1.getDate()==date2.getDate()){
-   
-    return true;
-}else{
-   
-    return false;
+    const currentDate = new Date(date);
+    const day = currentDate.getUTCDay();
+    const difference = day === 0 ? 6 : 6 - day; // Saturday (6) is the end of the week
+    currentDate.setUTCDate(currentDate.getUTCDate() + difference);
+    currentDate.setUTCHours(23, 59, 59, 999); // Set time to the end of the day in UTC
+    return currentDate;
 }
 
-}
-
-function dayCalculator(date){
-    var date2=new Date(date)
-    let day=date2.getDay();
-    var dayn;
-    switch(day){
-        case 0:
-            dayn='sun';
-            break;
-        case 1:
-            dayn='mon';
-            break;
-        case 2:
-            dayn='tue';
-            break;
-        case 3:
-            dayn='wed';
-            break;
-        case 4:
-            dayn='thu';
-            break;
-        case 5:
-           dayn='fri';
-                break;
-        case 6:
-            dayn='sat';
-                break; 
-        default:
-            break;       
-
-    }
-    return dayn;
-}
-function outOfRange(date){
-   var toconf=new Date(date);
-    var conf=new Date();
-    var conf2=new Date();
-    conf2.setDate(conf2.getDate()+7);
-    conf2=getEndOfTheWeek(conf2);
-    conf=getStartOfTheWeek(conf);
-    //console.log(toconf.getDay());
-    conf.setDate(conf.getDate()-1);
-    if((toconf.getTime()<conf.getTime())||(toconf.getTime()>conf2.getTime())){
-     return true;
-    }else{
-     return false;
-    }
- }
-
- function formatUsername(email) {
+function formatUsername(email) {
     if (!email) return '';
     let [name, domain] = email.split('@');
     let [firstName, lastName] = name.split('.');
@@ -174,63 +124,20 @@ function capitalize(word) {
     if (!word) return '';
     return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
 }
-
-//fine funzioni potenzialmente funzionanti
-//inserire connessione a DB qui 
-
-//inizio prova
-
-
-
-document.getElementById('downloadBtn').addEventListener('click', downloadCSV);
-
-document.getElementById('downloadBtn').addEventListener('click', downloadCSV);
-
-function downloadCSV() {
-    const currentWeekTable = document.querySelector('#currentWeek table');
-    const nextWeekTable = document.querySelector('#nextWeek table');
-
-    let csv = [];
-    
-    // Funzione per estrarre i dati da una tabella e formattarli come CSV
-    function extractTableData(table, title) {
-        let rows = table.querySelectorAll('tr');
-        csv.push(title);  // Aggiungi un titolo per la sezione della tabella
-        rows.forEach((row, rowIndex) => {
-            let rowData = [];
-            row.querySelectorAll('th, td').forEach(cell => {
-                let cellText = cell.innerText.replace(/,/g, '');  // Rimuovi eventuali virgole
-                rowData.push(`"${cellText}"`); // Aggiungi virgolette per gestire il testo lungo
-            });
-            csv.push(rowData.join(','));
-        });
-        csv.push('');  // Aggiungi una riga vuota per separare le tabelle
-    }
-
-    // Estrai i dati della tabella della settimana corrente
-    extractTableData(currentWeekTable, 'Settimana Corrente');
-
-    // Estrai i dati della tabella della prossima settimana
-    extractTableData(nextWeekTable, 'Settimana Successiva');
-
-    // Unisce tutte le linee in un'unica stringa separata da caratteri di nuova linea
-    let csvContent = csv.join('\n');
-
-    // Crea un blob e scaricalo
-    let blob = new Blob([csvContent], { type: 'text/csv' });
-    let url = URL.createObjectURL(blob);
-    let a = document.createElement('a');
-    a.setAttribute('href', url);
-    a.setAttribute('download', 'calendario_settimanale.csv');
-    a.click();
-    URL.revokeObjectURL(url);
+function formatDateToLocalString(date) {
+    return new Date(date.getTime() - (date.getTimezoneOffset() * 60000)).toLocaleDateString();
 }
+// Funzione per aggiornare il calendario
+async function updateCalendar(weekStart) {
+    const weekEnd = getEndOfTheWeek(weekStart);
+    const weekStartLocalString = formatDateToLocalString(weekStart);
+    const weekEndAdjusted = new Date(weekEnd.getTime() - 24 * 60 * 60 * 1000); // Subtract one day
+    const weekEndLocalString = formatDateToLocalString(weekEndAdjusted);
+    document.getElementById('weekDates').innerText = `Week from ${weekStartLocalString} to ${weekEndLocalString}`;
+    const calendarContainer = document.getElementById('calendarContainer');
+    calendarContainer.innerHTML = createCalendarHTML('current');
 
-
-// Esempio di utilizzo: caricare i task quando la pagina è pronta
-document.addEventListener('DOMContentLoaded', async () => {
     var username = await connector();
-    console.log("Il mio username ha valore: ", username);
     if (!username) {
         console.error('Username non trovato o errore durante la connessione.');
         return;
@@ -241,65 +148,115 @@ document.addEventListener('DOMContentLoaded', async () => {
         return;
     }
 
+    console.log('Tasks fetched:', tasks);
+
     for (var k = 0; k < tasks.length; k++) {
         let taskDate = new Date(tasks[k].day);
-        taskDate.setDate(taskDate.getDate() - 1);
+        taskDate.setUTCHours(0, 0, 0, 0); // Normalize the hour part of the date to UTC
+        console.log(`Task Date: ${taskDate}, Task:`, tasks[k]);
 
-        if (!outOfRange(taskDate)) {
+        // Verify if the task belongs to the current week
+        if (taskDate >= weekStart && taskDate <= weekEnd) {
+            console.log(`Task ${tasks[k].day} is within the current week.`);
             var startHour = tasks[k].start;
             var endHour = tasks[k].end;
 
-            if (startHour < endHour) {
-                // Turno nello stesso giorno
-                for (var y = startHour; y < endHour && y < 24; y++) {
-                    if (isCurrent(taskDate)) {
-                        addTask('current', dayCalculator(taskDate), y, username);
-                    } else {
-                        addTask('next', dayCalculator(taskDate), y, username);
-                    }
-                }
-            } else {
-                // Turno che attraversa la mezzanotte
-                for (var y = startHour; y < 24; y++) {
-                    if (isCurrent(taskDate)) {
-                        addTask('current', dayCalculator(taskDate), y, username);
-                    } else {
-                        addTask('next', dayCalculator(taskDate), y, username);
-                    }
-                }
-
-                // Incrementa la data per il giorno successivo
-                taskDate.setDate(taskDate.getDate() + 1);
-
-                for (var y = 0; y < endHour && y < 24; y++) {
-                    if (isCurrent(taskDate)) {
-                        addTask('current', dayCalculator(taskDate), y, username);
-                    } else {
-                        addTask('next', dayCalculator(taskDate), y, username);
-                    }
+            for (var y = startHour; y < endHour && y < 24; y++) {
+                // Add the task only if the cell ID is valid
+                let dayId = dayCalculator(taskDate);
+                console.log(dayId);
+                if (dayId) {
+                    addTask('current', dayId, y, username);
+                } else {
+                    console.warn(`Giorno non valido per la data ${taskDate}`);
                 }
             }
+        } else {
+            console.log(`Task non aggiunto alla settimana corrente: ${tasks[k].day}`);
         }
     }
+}
+function createCalendarHTML(calendar) {
+    let days = ['sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat']; // Modifica l'ordine dei giorni
+    let weekHTML = `
+        <table>
+            <thead>
+                <tr>
+                    <th>Ora</th>
+                    <th>Sunday</th>
+                    <th>Monday</th>
+                    <th>Tuesday</th>
+                    <th>Wednesday</th>
+                    <th>Thursday</th>
+                    <th>Friday</th>
+                    <th>Saturday</th>
+                </tr>
+            </thead>
+            <tbody>`;
+
+    for (let hour = 0; hour < 24; hour++) {
+        weekHTML += `<tr><td>${hour}:00 - ${hour + 1}:00</td>`;
+        for (let day of days) {
+            weekHTML += `<td id="${day}-${calendar}-${hour}"></td>`;
+        }
+        weekHTML += `</tr>`;
+    }
+
+    weekHTML += `</tbody></table>`;
+    return weekHTML;
+}
+function dayCalculator(date) {
+    let day = date.getDay();
+    switch (day) {
+        case 0: return 'sun';
+        case 1: return 'mon';
+        case 2: return 'tue';
+        case 3: return 'wed';
+        case 4: return 'thu';
+        case 5: return 'fri';
+        case 6: return 'sat';
+        default: return '';
+    }
+}
+
+function downloadCSV() {
+    const calendarContainer = document.querySelector('#calendarContainer table');
+    let csv = [];
+
+    let rows = calendarContainer.querySelectorAll('tr');
+    rows.forEach((row, rowIndex) => {
+        let rowData = [];
+        row.querySelectorAll('th, td').forEach(cell => {
+            let cellText = cell.innerText.replace(/,/g, '');  // Rimuovi eventuali virgole
+            rowData.push(`"${cellText}"`); // Aggiungi virgolette per gestire il testo lungo
+        });
+        csv.push(rowData.join(','));
+    });
+
+    let csvContent = csv.join('\n');
+    let blob = new Blob([csvContent], { type: 'text/csv' });
+    let url = URL.createObjectURL(blob);
+    let a = document.createElement('a');
+    a.setAttribute('href', url);
+    a.setAttribute('download', 'calendario_settimanale.csv');
+    a.click();
+    URL.revokeObjectURL(url);
+}
+
+// Event listener per il download del CSV
+document.getElementById('downloadBtn').addEventListener('click', downloadCSV);
+
+document.getElementById('nextWeekBtn').addEventListener('click', async () => {
+    currentWeekStart.setDate(currentWeekStart.getDate() + 7);
+    await updateCalendar(currentWeekStart);
 });
-//fine prova
 
-
-
-
-
-
-
-document.getElementById('nextWeekBtn').addEventListener('click', () => {
-    document.getElementById('currentWeek').style.display = 'none';
-    document.getElementById('nextWeek').style.display = 'block';
-    document.getElementById('nextWeekBtn').style.display = 'none';
-    document.getElementById('prevWeekBtn').style.display = 'inline-block';
+document.getElementById('prevWeekBtn').addEventListener('click', async () => {
+    currentWeekStart.setDate(currentWeekStart.getDate() - 7);
+    await updateCalendar(currentWeekStart);
 });
 
-document.getElementById('prevWeekBtn').addEventListener('click', () => {
-    document.getElementById('currentWeek').style.display = 'block';
-    document.getElementById('nextWeek').style.display = 'none';
-    document.getElementById('nextWeekBtn').style.display = 'inline-block';
-    document.getElementById('prevWeekBtn').style.display = 'none';
+// Funzione iniziale per caricare il calendario corrente
+document.addEventListener('DOMContentLoaded', () => {
+    updateCalendar(currentWeekStart);
 });
